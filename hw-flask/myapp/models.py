f635f3dca93284d56fb5955d90665250552c396b
Vladislav Semenyutin <50467295+skiffin-git@users.noreply.github.com>
2021-11-01 19:17:46 -0700

    Delete hw-flask directory

--- hw-flask/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from myapp import db
-class City(db.Model):
-    """docstring for City."""
-    city_name = db.Column(db.String(64), primary_key=True)
-    city_rank = db.Column(db.Integer)
-    is_visited = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return f'<City {self.city_name}>'
